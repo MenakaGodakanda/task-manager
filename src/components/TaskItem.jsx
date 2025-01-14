c80293b9c28d3e00a3cc3d2ccb6d16917852617f
Menaka Godakanda <156875412+MenakaGodakanda@users.noreply.github.com>
2025-01-15 04:22:44 +0800

    Add files via upload

---
+++ src/components/TaskItem.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TaskItem = ({ task, deleteTask, toggleTaskCompletion }) => {
+  return (
+    <li>
+      <span
+        style={{
+          textDecoration: task.completed ? "line-through" : "none",
+        }}
+      >
+        {task.text}
+      </span>
+      <button onClick={() => toggleTaskCompletion(task.id)}>
+        {task.completed ? "Undo" : "Complete"}
+      </button>
+      <button onClick={() => deleteTask(task.id)}>Delete</button>
+    </li>
+  );
+};
+
+export default TaskItem;
+
